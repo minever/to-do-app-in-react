32670ccd13f0bac4935f2828951e1d9b1cdb521e
minever <minever_1997@abv.bg>
2020-03-09 12:49:17 +0200

    Updates

---
+++ precache-manifest.71fb45c446a0d21025f1216581203fbc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49f3784a26deeeabdc42680a23ede8c3",
+    "url": "/to-do-app-in-react/index.html"
+  },
+  {
+    "revision": "e80416f28b46c8b5d0ff",
+    "url": "/to-do-app-in-react/static/css/main.567d08fb.chunk.css"
+  },
+  {
+    "revision": "67a38ad5d5bea9c14343",
+    "url": "/to-do-app-in-react/static/js/2.2a79b781.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/to-do-app-in-react/static/js/2.2a79b781.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e80416f28b46c8b5d0ff",
+    "url": "/to-do-app-in-react/static/js/main.e94b1bc4.chunk.js"
+  },
+  {
+    "revision": "ff67fa2289972f2f2c13",
+    "url": "/to-do-app-in-react/static/js/runtime-main.2bd3317a.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/to-do-app-in-react/static/media/logo.5d5d9eef.svg"
+  }
+]);
